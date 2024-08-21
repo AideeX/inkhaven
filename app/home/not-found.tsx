f0202fe79a1242b192edc5f21bb91e5cde69d37a
Idaresit Ekpoh-Ubachukwu <aideeubachukwu@gmail.com>
2024-08-21 18:20:35 +0100

    Update not-found pages

--- app/home/not-found.tsx
+++ app/home/not-found.tsx
@@ -1,20 +1,28 @@
 'use client';
 
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import { playfairDisplay } from '@/app/ui/styles/fonts';
+import { playfairDisplay } from './ui/styles/fonts';
 
 export default function Custom404() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-light-primary dark:bg-dark-primary">
-      <div className="text-center p-8">
-        <Image src="/under-construction.svg" alt="404 illustration" width={500} height={500} className='justify-center'/>
-        <h1 className={`${playfairDisplay.className} text-6xl font-bold text-light-heading dark:text-dark-heading mb-4`}>404</h1>
-        <p className="text-2xl text-light-text dark:text-dark-text mb-6">
-                Oops! The page you are looking for doesn&apos;t exist.
-        </p>
-        <Link href="/home" className="text-dark-text dark:text-dark-linkDefault hover:text-dark-text dark:hover:text-dark-linkHover bg-light-buttonDefault dark:bg-dark-buttonDefault py-2 px-4 rounded-md">Go Home</Link>
-      </div>
-    </div>
-  );
+    const router = useRouter();
+
+    const handleGoHome = () => {
+        router.replace('/home');
+    };
+
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-light-primary dark:bg-dark-primary">
+            <div className="text-center p-8">
+                <Image src="/under-construction.svg" alt="404 illustration" width={500} height={500} className="justify-center" />
+                <h1 className={`${playfairDisplay.className} text-6xl font-bold text-light-heading dark:text-dark-heading mb-4`}>404</h1>
+                <p className="text-2xl text-light-text dark:text-dark-text mb-6">
+                    Oops! The page you are looking for doesn&apos;t exist.
+                </p>
+                <button onClick={handleGoHome} className="text-dark-text dark:text-dark-linkDefault hover:text-dark-text dark:hover:text-dark-linkHover bg-light-buttonDefault dark:bg-dark-buttonDefault py-2 px-4 rounded-md">
+                    Go Home
+                </button>
+            </div>
+        </div>
+    );
 }

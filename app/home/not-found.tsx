c90ed4716f1c67a1f890cfcdeaa38de94f1faf0e
Idaresit Ekpoh-Ubachukwu <aideeubachukwu@gmail.com>
2024-08-21 19:59:16 +0100

    Update not-found.tsx

--- app/home/not-found.tsx
+++ app/home/not-found.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import { playfairDisplay } from './ui/styles/fonts';
+import { playfairDisplay } from '@/app/ui/styles/fonts';
 
 export default function Custom404() {
     const router = useRouter();
